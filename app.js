97c9d7f0676ed7f3582bf7c8bb0fb9604742196b
mllrjo <159646777+mllrjo@users.noreply.github.com>
2024-12-20 14:35:17 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// I attack target [ I become target ]
+//  if target survives [ target becomes attacker ]
+//      target attacks me
+//         if I survive {
+                }
+//  } else {
+//    retreat
+//    if there is another target
+//        attack target
+//    else if no more targets
+//        I win.
+//    }
+//  }
+// }
